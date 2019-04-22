ec717906379b71215e426699481446cc5e500ac5
Przemyslaw Prencel <przemyslaw.prencel@goodylabs.com>
2019-04-22 15:58:34 +0200

    isolation issuanceMethodCallProductGetTypeTwice

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -65,4 +65,17 @@ public class BookKeeperTest {
 
         Assert.assertThat(invoice.getItems().get(0).getProduct().getType(), Matchers.equalTo(ProductType.DRUG));
     }
+
+    @Test public void issuanceMethodCallProductGetTypeTwice(){
+        Money money = new Money(2, Money.DEFAULT_CURRENCY);
+
+        RequestItem requestItem1 = new RequestItem(productData, 1, money);
+        RequestItem requestItem2 = new RequestItem(productData, 1, money);
+
+        invoiceRequest.add(requestItem1);
+        invoiceRequest.add(requestItem2);
+        bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        verify(productData, times(2)).getType();
+    }
 }
